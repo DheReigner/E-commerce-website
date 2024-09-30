ff0d253744b21fca510c27c82f1befd50f45b0c0
DheReigner <dhereigner@gmail.com>
2024-09-30 16:39:23 +0300

    Your commit message

--- E-COMMERCE WEBSITE/index.php
+++ E-COMMERCE WEBSITE/index.php
@@ -2,7 +2,7 @@
 <html lang="en">
 
 <head>
-    <!-- Google tag (gtag.js) -->
+    Google tag (gtag.js)
     <script async src="https://www.googletagmanager.com/gtag/js?id=G-M1DXJ2F7EQ"></script>
     <script>
         window.dataLayer = window.dataLayer || [];
@@ -14,6 +14,27 @@
 
         gtag('config', 'G-M1DXJ2F7EQ');
     </script>
+    <!-- Google Tag Manager -->
+    <script>
+        (function(w, d, s, l, i) {
+            w[l] = w[l] || [];
+            w[l].push({
+                'gtm.start': new Date().getTime(),
+                event: 'gtm.js'
+            });
+            var f = d.getElementsByTagName(s)[0],
+                j = d.createElement(s),
+                dl = l != 'dataLayer' ? '&l=' + l : '';
+            j.async = true;
+            j.src =
+                'https://www.googletagmanager.com/gtm.js?id=' + i + dl;
+            f.parentNode.insertBefore(j, f);
+        })(window, document, 'script', 'dataLayer', 'GTM-P5XNKQGN');
+    </script>
+    <!-- End Google Tag Manager -->
+
+
+
 
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
